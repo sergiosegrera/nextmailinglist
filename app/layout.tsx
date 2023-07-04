d1e4abb25e161e0df2afe897385868216c4160e9
Sergio Segrera <sergioarturosegrera@gmail.com>
2023-07-04 13:06:41 -0400

    Fix Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <GoogleAnalytics key={process.env.NEXT_PUBLIC_GA_TRACKING_ID} />
       <body
         className={`${inter.className} min-h-[100dvh] flex flex-col justify-between container mx-auto`}
       >
--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,19 +1,23 @@
 "use client";
 import Script from "next/script";
 
-export default function GoogleAnalytics({}) {
+export default function GoogleAnalytics({ key }: { key: string | undefined }) {
+  if (!key) {
+    return null;
+  }
+
   return (
     <>
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${key}`}
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          gtag('config', '${key}', {
             page_path: window.location.pathname,
           });
         `}
